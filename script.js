57bf55753cd2c2cebc9cd851305c6305f42b56a7
ikotsov <i.kotsovilis@gmail.com>
2021-12-13 15:36:14 +0200

    add: log for Portugal data

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 class Countries {
-  url = "https://restcountries.com/v3.1/all";
+  portugalUrl = "https://restcountries.com/v3.1/name/portugal";
   countries = [];
 
   async fetch() {
-    const response = await fetch(this.url);
-    const json = await response.json();
-    this.countries = json;
-    console.log(this.countries[0].name.common); // Should log `Kenya`
+    const response = await fetch(this.portugalUrl);
+    const [data] = await response.json();
+    this.countries.push(data);
+    this.logData();
+  }
+
+  logData() {
+    console.log(this.countries[0].name.common); // name
+    console.log(this.countries[0].currencies.EUR.name); // currency
+    console.log(this.countries[0].region); // where it belongs
+    console.log(this.countries[0].languages.por); // spoken language
+    console.log(this.countries[0].population); // people
+    console.log(this.countries[0].flags.svg); // icon
   }
 }
 const countries = new Countries();
